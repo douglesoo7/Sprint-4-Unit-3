c298a231a1c9b06a288ce195010f025b4d8e0d2a
Dougles <maddysachin786@gmail.com>
2021-09-27 14:40:18 +0530

    Second Commit

--- app/src/main/java/com/example/sprint4unit3/SongsViewHolder.kt
+++ app/src/main/java/com/example/sprint4unit3/SongsViewHolder.kt
@@ -4,5 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class SongsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun setData(responseDTO: ResponseDTO){
 
+    }
 }

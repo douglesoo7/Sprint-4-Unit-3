856afa5cdf75e66d74bb4f721824334838543ca2
Dougles <maddysachin786@gmail.com>
2021-09-27 17:25:50 +0530

    Seventh Commit

---
+++ app/src/main/java/com/example/sprint4unit3/MusicPlayerService.kt
@@ -0,0 +1,14 @@
+package com.example.sprint4unit3
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.core.app.JobIntentService
+
+class MusicPlayerService : JobIntentService() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+    override fun onHandleWork(intent: Intent) {
+
+    }
+}
